c331babb8aec72fe16dc24be0a19fa453ccb0606
Macir <mj.brustolin@hotmail.com>
2022-01-13 15:53:54 -0300

    Adicionar arquivos de projeto.

---
+++ Desafios-Diversos/DesafiosLocalizaLabs/HoHoHo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desafios_Diversos.Desafios_Localiza_Labs {
+    class HoHoHo {
+
+        public static void Executar() {
+
+            Console.WriteLine("Insira um número inteiro: ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++) {
+                if (i < N - 1) {
+                    Console.Write("Ho ");                  //Complete o código no espaço em branco
+                } else {
+                    Console.WriteLine("Ho!");
+                }
+            }
+        }
+    }
+}
+
---
+++ Desafios-Diversos/Program.cs
@@ -0,0 +1,18 @@
+﻿using Desafios_Diversos.Desafios_Localiza_Labs;
+using System;
+using System.Collections.Generic;
+
+namespace Desafios_Diversos {
+    class Program {
+        static void Main(string[] args) {
+
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"Desafio do Papai Noel", HoHoHo.Executar},
+            });
+
+            central.SelecionarEExecutar();
+
+        }
+
+    }
+}
