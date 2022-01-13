dde175d98e49104b655e6b679ec9cbf0e1b46a41
Macir <mj.brustolin@hotmail.com>
2022-01-13 16:02:55 -0300

    Algumas correções de sintaxe

--- Desafios-Diversos/DesafiosLocalizaLabs/DividindoXporY.cs
+++ Desafios-Diversos/DesafiosLocalizaLabs/DividindoXporY.cs
@@ -23,3 +23,4 @@ namespace Desafios_Diversos.DesafiosLocalizaLabs {
             }
         }
     }
+}
--- Desafios-Diversos/DesafiosLocalizaLabs/PedroBento.cs
+++ Desafios-Diversos/DesafiosLocalizaLabs/PedroBento.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Desafios_Diversos.DesafiosLocalizaLabs {
